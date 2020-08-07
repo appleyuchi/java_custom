2b0c4aa7e56cedb02636a53674ed4cfb9222aa16
appleyuchi <appleyuchi@tom.com>
2020-08-07 21:44:37 +0800

    增加了一些自定义文件

---
+++ Range.java
@@ -0,0 +1,40 @@
+import java.util.Iterator;
+
+class Range implements Iterable<Integer> {
+    private final int start;
+    private final int end;
+    private final int step;
+
+    public Range(int end) {
+        this(0, end, 1);
+    }
+
+    public Range(int start, int end) {
+        this(start, end, 1);
+    }
+
+    public Range(int start, int end, int step) {
+        this.start = start;
+        this.end = end;
+        this.step = step;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Itr();
+    }
+    private class Itr implements Iterator<Integer> {
+        int current = start;
+        @Override
+        public boolean hasNext() {
+            return step > 0 ? current < end : current > end;
+        }
+
+        @Override
+        public Integer next() {
+            int t = current;
+            current += step;
+            return t;
+        }
+    }
+}
